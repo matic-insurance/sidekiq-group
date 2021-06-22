985f8f5c80f127ae5864a69fbca7167cdf60892f
Oleh Maikovych <18499500+omaik@users.noreply.github.com>
2021-06-22 16:31:27 +0300

    mutant: wip

--- spec/sidekiq/group_spec.rb
+++ spec/sidekiq/group_spec.rb
@@ -1,8 +1,16 @@
 class FakeGroupWorker
   include Sidekiq::Group
+  attr_accessor :cid_value
 
-  def valid_perform
+  def valid_perform_with_args
     sidekiq_group(id: '12345') do |group|
+      group.add('54321')
+      self.cid_value = Thread.current[:group_collection]
+    end
+  end
+
+  def valid_perform_no_args
+    sidekiq_group do |group|
       group.add('54321')
     end
   end
@@ -28,12 +36,31 @@ RSpec.describe Sidekiq::Group do
   end
 
   describe 'successful scenario' do
-    before { worker.valid_perform }
+    context 'when arguments are passed' do
+      before { worker.valid_perform_with_args }
 
-    it { expect(collection).to have_received(:callback_options=).with(id: '12345') }
-    it { expect(collection).to have_received(:callback_class=).with('FakeGroupWorker') }
-    it { expect(collection).to have_received(:add).with('54321') }
-    it { expect(collection).to have_received(:spawned_jobs!) }
+      it { expect(collection).to have_received(:callback_options=).with(id: '12345') }
+      it { expect(collection).to have_received(:callback_class=).with('FakeGroupWorker') }
+      it { expect(collection).to have_received(:add).with('54321') }
+      it { expect(collection).to have_received(:spawned_jobs!) }
+
+      it 'sets group_collection during yield' do
+        expect(worker.cid_value).to be_present
+      end
+
+      it 'cleans group_collection afterwards' do
+        expect(Thread.current[:group_collection]).to eq(nil)
+      end
+    end
+
+    context 'when arguments arent passed' do
+      before { worker.valid_perform_no_args }
+
+      it { expect(collection).to have_received(:callback_options=).with({}) }
+      it { expect(collection).to have_received(:callback_class=).with('FakeGroupWorker') }
+      it { expect(collection).to have_received(:add).with('54321') }
+      it { expect(collection).to have_received(:spawned_jobs!) }
+    end
 
     context 'when on_complete is not defined' do
       let(:logger) { Sidekiq::Logging.logger }
